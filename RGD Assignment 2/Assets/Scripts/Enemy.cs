8c0c739d9994f0181a94bbb2f8ecd5fee966c1be
ArrowNate <George.Edmon@triosstudent.com>
2023-05-04 22:11:05 -0400

    Added Enemy and Stairs and Fixed Bullet and Bullet Rotation

--- RGD Assignment 2/Assets/Scripts/Enemy.cs
+++ RGD Assignment 2/Assets/Scripts/Enemy.cs
@@ -2,25 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     public Transform bulletSpawnPoint;
     public GameObject bullet;
 
-    public GameObject bulletSpawn;
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("Shooting", 4, 1);
+    }
+
+    void Shooting()
+    {
+        Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Instantiate(bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-        }
+        
     }
 }
